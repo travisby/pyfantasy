f0371342dee42793c28aaca40ff9cfc52114f6df
Travis Beatty <travisby@gmail.com>
2013-05-09 14:51:03 -0400

    Greenlit ensuring api object is of type api.Api

--- pyfantasy/api_wrapper.py
+++ pyfantasy/api_wrapper.py
@@ -1,8 +1,11 @@
+import api
+
+
 class Api(object):
     _api_key = ''
     _api = None
 
     def __init__(self, api_key):
-        self._api = None
+        self._api = api.Api('')
 
         self._api_key = api_key
