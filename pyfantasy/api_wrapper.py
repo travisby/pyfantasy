d16dad022fdfc74118252a8fd61c0d8d97444310
Travis Beatty <travisby@gmail.com>
2013-05-09 14:53:10 -0400

    Greenlit ensure correct api url

--- pyfantasy/api_wrapper.py
+++ pyfantasy/api_wrapper.py
@@ -2,10 +2,12 @@ import api
 
 
 class Api(object):
+    BASE_URL = 'http://api.fantasyfootballnerd.com'
+
     _api_key = ''
     _api = None
 
     def __init__(self, api_key):
-        self._api = api.Api('')
+        self._api = api.Api(self.BASE_URL)
 
         self._api_key = api_key
