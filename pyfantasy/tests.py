4dec5819b7f4dd564ba5a0a1fe1fd5025e65b240
Travis Beatty <travisby@gmail.com>
2013-05-09 14:38:34 -0400

    Refactored the Test class

--- pyfantasy/tests.py
+++ pyfantasy/tests.py
@@ -17,11 +17,21 @@ class TestAPIFunctions(MyUnitTestCase):
     def setUp(self):
         self._key = 'thisismykey'
 
+        self.api = api_wrapper.Api(self._key)
+
+        self._endpoint_keys = [
+            'season_schedule',
+            'week_projections',
+            'week_injuries',
+            'players',
+            'player',
+            'player_draft_rankings',
+        ]
+
     def tearDown(self):
         pass
 
     def test_init_saves_api_key(self):
-        api = api_wrapper.Api(self._key)
-        self.assertEqual(api._api_key, self._key)
+        self.assertEqual(self.api._api_key, self._key)
 
 unittest.main()
