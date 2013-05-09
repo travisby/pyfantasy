c21a24d8fef6c7c35d71ca70e01a90326cea53fe
Travis Beatty <travisby@gmail.com>
2013-05-09 12:11:07 -0400

    Added tests file

---
+++ pyfantasy/tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestAPIFunctions(unittest.TestCase):
+    """Tests the interface with pyrest"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
