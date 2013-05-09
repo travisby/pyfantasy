86b28fa75e34f9eaa2135fd56dee8963c277665a
Travis Beatty <travisby@gmail.com>
2013-05-09 15:02:10 -0400

    Greenlit ensuring endpoints were correct

--- pyfantasy/api_wrapper.py
+++ pyfantasy/api_wrapper.py
@@ -3,11 +3,20 @@ import api
 
 class Api(object):
     BASE_URL = 'http://api.fantasyfootballnerd.com'
+    ENDPOINTS = {
+        'season_schedule': 'ffnScheduleXML.php',
+        'week_projections': 'ffnSitStartXML.php',
+        'week_injuries': 'ffnInjuriesXML.php',
+        'players': 'ffnPlayersXML.php',
+        'player': 'ffnPlayerDetailsXML.php',
+        'player_draft_rankings': 'ffnRankingsXML.php',
+    }
 
     _api_key = ''
     _api = None
 
     def __init__(self, api_key):
         self._api = api.Api(self.BASE_URL)
+        self._api.update_endpoints(self.ENDPOINTS)
 
         self._api_key = api_key
