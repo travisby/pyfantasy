252627eb8fcca351335662e8f86b44685ba29777
Travis Beatty <travisby@gmail.com>
2013-05-09 14:08:13 -0400

    Added test for Api.__init__
    To ensure that we can pass the api key in to the constructor

--- pyfantasy/tests.py
+++ pyfantasy/tests.py
@@ -1,7 +1,16 @@
 import unittest
+import api_wrapper
 
 
-class TestAPIFunctions(unittest.TestCase):
+class MyUnitTestCase(unittest.TestCase):
+    def assertNotRaises(self, code, exc):
+        try:
+            code()
+        except exc:
+            self.fail('%s did raise %s' % (code.func_name, exc))
+
+
+class TestAPIFunctions(MyUnitTestCase):
     """Tests the interface with pyrest"""
 
     def setUp(self):
@@ -9,3 +18,9 @@ class TestAPIFunctions(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_init_takes_api_key(self):
+        my_function = lambda: api_wrapper.Api('3')
+        self.assertNotRaises(my_function, Exception)
+
+unittest.main()
